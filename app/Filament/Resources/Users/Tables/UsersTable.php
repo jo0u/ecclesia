4bc375ac73dbdf67aa2b1b54230ca08746251e59
jou.junior <joualexandre120@hotmail.com>
2025-10-09 16:47:48 -0300

    feat(users): Criação de usuários e permissões
    Esta alteração introduz o módulo de criação de usuários, incluindo:
    - Implementação da rota e lógica para criar um novo usuário.
    - Integração com a tabela de usuários no banco de dados.
    - Adição de um campo para definir permissões iniciais para o novo usuário.

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Users\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -36,7 +35,6 @@ class UsersTable
                 //
             ])
             ->recordActions([
-                ViewAction::make(),
                 EditAction::make(),
             ])
             ->toolbarActions([
